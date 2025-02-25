26a0bed107a0aebd4cb37ad121db93f0b5017848
Sumit Panda <sumit.panda@bigbinary.com>
2025-02-25 21:40:28 +0530

    Added validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  MAX_TITLE_LENGTH = 125
+  MAX_DESCRIPTION_LENGTH = 10000
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+  validates :description, presence: true, length: { maximum: MAX_DESCRIPTION_LENGTH }
   validates :upvotes, presence: true, numericality: true
   validates :downvotes, presence: true, numericality: true
   validates :is_bloggable, presence: true, inclusion: { in: [true, false] }
