48d72a898937fc6bf08ea0bdd9e9ac0c4b505a7c
Sumit Panda <sumit.panda@bigbinary.com>
2025-03-21 15:06:10 +0530

    Added secrets.yml and configured sidekiq.rb file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+if Rails.env.test?
+  require "sidekiq/testing"
+  Sidekiq::Testing.inline!
+end
+
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"], size: 4, network_timeout: 5 }
+  config.redis = { url: Rails.application.secrets.redis_url, size: 9 } unless Rails.env.test? || Rails.env.production?
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], size: 4, network_timeout: 5 }
+  config.redis = { url: Rails.application.secrets.redis_url, size: 9 }
 end
